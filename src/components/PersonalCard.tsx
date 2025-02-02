758e0f23aa80940407d087f312845c84f4d6f9ed
Dunn, Simeon <simedunn01@gmail.com>
2025-02-01 21:58:15 -0500

    add show more/less label

--- src/components/PersonalCard.tsx
+++ src/components/PersonalCard.tsx
@@ -1,5 +1,5 @@
 import PersonalLink from "./PersonalLink";
-import { createSignal } from "solid-js";
+import { createSignal, Show } from "solid-js";
 import { AiFillGithub, AiFillLinkedin } from 'solid-icons/ai'
 import { FiMail } from "solid-icons/fi";
 import { IoLocationOutline } from 'solid-icons/io';
@@ -12,8 +12,9 @@ export default function PersonalCard() {
     <section class="sidebar p-4 pb-0 md:px-8 md:pt-12 md:pb-3 flex flex-col gap-5 h-fit">
       <button
         onClick={() => setOpenLinks(prev => !prev)}
-        class="absolute block md:hidden top-4 right-4 text-yellow-300"
+        class="absolute flex gap-1 items-center md:hidden bottom-4 right-4 text-yellow-300"
       >
+        <span class="text-xs font-semibold"><Show when={openLinks()} fallback={<>show more</>}>show less</Show></span>
         <FaSolidAngleDown size={20} class={`transition-all ${openLinks() ? "rotate-180" : "rotate-0"}`} />
       </button>
 
